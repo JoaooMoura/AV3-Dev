7e7dbbe492213ef544faed64b40c4017ebc0e3fc
João Moura <jv.moura.sjc@gmail.com>
2025-11-30 02:04:16 -0300

    reset

--- backend/src/routes/metricas.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getMetricas, getMetricasAgregadas } from '../controllers/metricas.controller.js'
-
-const router = Router()
-
-router.get('/', getMetricas)
-router.get('/agregadas', getMetricasAgregadas)
-
-export default router
